2b6ad6b74d5b7b1b934be07f20d6a2bdfb8afc73
PhosPerphyllize <18813155299@163.com>
2022-01-17 14:21:13 +0800

    Update model_val_nor_seq.py

--- input64_16/model_val_nor_seq.py
+++ input64_16/model_val_nor_seq.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from read_data import *
 
-model_path = "nnrnn_save/rnn1_linear1/nnrnn_model600.pth"
+model_path = "nnrnn_save/rnn4_linear2/nnrnn_model3000.pth"
 # model_path = "nnlstm_save/nnlstm_model1200.pth"
 nn_model = torch.load(model_path)
 nn_model.to("cpu")
